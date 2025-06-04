0db9ef6a6ce24605e51dcce82c3c7bf64152044a
prof-ashleyevans <130587462+prof-ashleyevans@users.noreply.github.com>
2025-06-04 16:09:54 -0400

    Make 5-star icon bigger in the Reviews Section

--- app/Reviews.jsx
+++ app/Reviews.jsx
@@ -79,8 +79,8 @@ export default function Reviews() {
 
                                 {/* Content */}
                                 <div className="relative z-10 h-full flex flex-col items-center justify-center px-6 text-center">
-                                    <img src="/images/5 Stars.png" alt="5 stars" className="h-30 md:h-13 mt-2 object-contain" />
-                                    <p className="italic text-xl md:text-2xl mb-12 pt-40">"{review.quote}"</p>
+                                    <img src="/images/5 Stars.png" alt="5 stars" className="h-10 md:h-10 mt-2 object-contain" />
+                                    <p className="italic text-2xl md:text-2xl mb-12 pt-40">"{review.quote}"</p>
                                     {/*} <p className="text-yellow-300 text-md mt-1">&ndash; {review.source}</p>*/}
                                     <img src={review.logo} alt={review.source} className="h-24 mt-12 object-contain" />
                                 </div>
