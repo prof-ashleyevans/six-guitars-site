46797b9e291358dfee21918ee39be06bb8b79de3
prof-ashleyevans <130587462+prof-ashleyevans@users.noreply.github.com>
2025-06-16 19:24:13 -0400

    Hero Image Calibration (1)

--- app/CharacterGridSection.jsx
+++ app/CharacterGridSection.jsx
@@ -51,25 +51,38 @@ export default function CharacterGridSection() {
     return (
         <div className="flex flex-col w-full">
             <section
-                className="relative w-full h-auto sm:min-h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat mt-25 pt-90 pb-4 sm:pt-0 sm:pb-0"
+                className="relative w-full h-auto sm:min-h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat mt-5 pt-150 pb-4 sm:pt-0 sm:pb-0"
                 style={{
                     backgroundImage: `url('/images/hero/pc/background.jpg')`,
                 }}
             >
 
                 {characterImages.map((char, i) => (
-                    <img
+                    <div
                         key={i}
-                        data-aos={char.aos}
-                        data-aos-delay={char.delay}
-                        src={isMobile ? char.mobileSrc : char.desktopSrc}
-                        alt={char.alt}
-                        className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
-      ${i === 0 ? 'w-screen max-w-none' : 'w-full max-w-screen-xl'} 
-      h-auto object-contain`}
+                        className={`
+      absolute left-1/2 transform -translate-x-1/2 
+      ${i === 2 ? 'top-[30%] sm:top-[45%]' : 'top-1/2 -translate-y-1/2'}
+      ${i === 1 ? 'top-[29%] sm:top-[52%]' : 'top-1/2 -translate-y-1/2'} 
+      ${i === 0 ? 'w-screen max-w-none h-[95vh] overflow-hidden' : ''} 
+      ${i !== 0 ? 'w-full max-w-screen-xl' : ''}
+    `}
                         style={{ zIndex: char.zIndex }}
-                    />
+                    >
+                        <img
+                            data-aos={char.aos}
+                            data-aos-delay={char.delay}
+                            src={isMobile ? char.mobileSrc : char.desktopSrc}
+                            alt={char.alt}
+                            className={i === 0
+                                ? 'w-full h-auto sm:h-full sm:object-cover sm:object-top'
+                                : 'w-full h-auto object-contain'}
+                        />
+                    </div>
                 ))}
+
+
+
             </section>
 
             {/* Content below hero */}
