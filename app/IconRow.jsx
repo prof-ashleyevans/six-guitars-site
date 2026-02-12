a38cb9f3433290a58fad34ee26c99f85e256fbc7
ashleyevans <ashley.n.berry@gmail.com>
2026-02-12 06:45:35 -0500

    Further adjust icon sizes and positioning for large screens
    - Reduce icon sizes for lg and xl screens (both now 56px)
    - Move icon row down for lg (mt-22), xl (mt-28), and 2xl (mt-30)
    - All changes remain PC-only, mobile unchanged

--- app/IconRow.jsx
+++ app/IconRow.jsx
@@ -16,13 +16,13 @@ const IconRow = () => {
 
     return (
         <div
-            className={`relative z-10 flex flex-nowrap justify-center gap-2 w-full max-w-screen-md px-4 mb-4 md:mt-12 lg:mt-20 xl:mt-24 2xl:mt-28 bg-transparent mx-auto text-center transition-opacity duration-1800 ease-out
+            className={`relative z-10 flex flex-nowrap justify-center gap-2 w-full max-w-screen-md px-4 mb-4 md:mt-12 lg:mt-22 xl:mt-28 2xl:mt-30 bg-transparent mx-auto text-center transition-opacity duration-1800 ease-out
         ${isVisible ? 'opacity-100' : 'opacity-0'}
     `}
         >
             {/* Icon 1 */}
             <div className="flex flex-col items-center w-22 sm:w-50 text-white flex-shrink-0">
-                <div className="relative w-10 h-10 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-16 lg:h-16 xl:w-20 xl:h-20 2xl:w-18 2xl:h-18 max-w-[80px] max-h-[80px]">
+                <div className="relative w-10 h-10 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-14 lg:h-14 xl:w-14 xl:h-14 2xl:w-16 2xl:h-16 max-w-[72px] max-h-[72px]">
                     <Image
                         src="/images/icons/guiar_icon.png"
                         alt="Guitar Icon"
@@ -37,7 +37,7 @@ const IconRow = () => {
 
             {/* Icon 2 */}
             <div className="flex flex-col items-center w-22 sm:w-50 text-white flex-shrink-0">
-                <div className="relative w-10 h-10 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-16 lg:h-16 xl:w-20 xl:h-20 2xl:w-18 2xl:h-18 max-w-[80px] max-h-[80px]">
+                <div className="relative w-10 h-10 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-14 lg:h-14 xl:w-14 xl:h-14 2xl:w-16 2xl:h-16 max-w-[72px] max-h-[72px]">
                     <Image
                         src="/images/icons/ticket_icon.png"
                         alt="Ticket Icon"
@@ -52,7 +52,7 @@ const IconRow = () => {
 
             {/* Icon 3 */}
             <div className="flex flex-col items-center w-22 sm:w-50 text-white flex-shrink-0">
-                <div className="relative w-10 h-10 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-16 lg:h-16 xl:w-20 xl:h-20 2xl:w-18 2xl:h-18 max-w-[80px] max-h-[80px] ml-5">
+                <div className="relative w-10 h-10 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-14 lg:h-14 xl:w-14 xl:h-14 2xl:w-16 2xl:h-16 max-w-[72px] max-h-[72px] ml-5">
                     <Image
                         src="/images/icons/Stories Icon.png"
                         alt="Stories Icon"
